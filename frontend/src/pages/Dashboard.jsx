b409b69714a53d083019cd120b2cf6f2700917a1
Illia Yevseienkov <yevseienkovimp@gmail.com>
2026-01-13 14:53:41 -0500

    Testing event fetching

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,29 @@
-function Dashboard(){
-    return(
+function Dashboard() {
+    const [events, setEvents] = useState([]);
+
+    useEffect(() => {
+        async function getEvent() {
+            const res = await fetch("http://localhost:3000/events", {
+                credentials: "include"
+            });
+            if(!res.ok){
+                return false;
+            }
+
+            const events = await res.json();
+            return events;
+        }
+
+        getEvent()
+          .then(data => {
+            setEvents(data);
+            console.log(data);
+        })
+    }, []);
+
+    return (
         <>
-          <h1>Dashboard</h1>
+            <h1>Dashboard</h1>
         </>
     );
 }
