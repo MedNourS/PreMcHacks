c270f968a3e7b62ca00f2236fb6b89578da21bb6
Illia Yevseienkov <yevseienkovimp@gmail.com>
2026-01-13 14:55:44 -0500

    Fixed useNavigate

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
+import { useNavigate } from "react-router-dom"
 
 function SignUpPage() {
+    const navigate = useNavigate();
     const [userInfo, setUserInfo] = useState({
         username: "",
         email: "",
